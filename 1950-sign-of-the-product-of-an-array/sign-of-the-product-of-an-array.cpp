e01479f7418fe784a6fc50755b2232b90f2c0e2c
uday kumar <challaudaykumar1@gmail.com>
2026-03-23 21:54:40 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (49.04%) - LeetSync

---
+++ 1950-sign-of-the-product-of-an-array/sign-of-the-product-of-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int p = 0;
+        int n = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                return 0;
+            }else if(nums[i] < 0){
+                n = n+1;
+            }
+        }
+         if(n % 2 == 0){
+            return 1;
+         }else{
+            return -1;
+         }
+        
+    }
+};
